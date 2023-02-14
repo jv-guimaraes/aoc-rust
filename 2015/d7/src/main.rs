8d1db0f875a3d41cd3ccd47796e4baa0015f8edf
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-02-14 02:05:41 -0300

    Improve value_of() readabilty and performance

--- 2015/d7/src/main.rs
+++ 2015/d7/src/main.rs
@@ -21,9 +21,8 @@ fn value_of(commands: &str, identifier: &str) -> u16 {
 
     for line in commands.lines().rev() {
         let tokens = line.split(' ').collect::<Vec<_>>();
-        let reciever = *tokens.last().unwrap();
         
-        if reciever != identifier { continue; }
+        if *tokens.last().unwrap() != identifier { continue; }
         
         if tokens.len() == 3 {
             if let Ok(num) = tokens[0].parse::<u16>() {
@@ -33,7 +32,7 @@ fn value_of(commands: &str, identifier: &str) -> u16 {
                 return value_of(commands, tokens[0])
             }
         }
-        else if line.contains("NOT") {
+        else if tokens.len() == 4  { // NOT
             let operand = tokens[1];
             if let Ok(num) = operand.parse::<u16>() {
                 insert_into_dict(identifier, !num);
@@ -42,22 +41,23 @@ fn value_of(commands: &str, identifier: &str) -> u16 {
                 return !value_of(commands, operand)
             }
         }
-        else if line.contains("AND") {
-            let (a, b) = (tokens[0], tokens[2]);
+
+        let operator = tokens[1];
+        let (a, b) = (tokens[0], tokens[2]);
+        
+        if operator == "AND" {
             let a = a.parse().unwrap_or_else(|_| value_of(commands, a));
             let b = b.parse().unwrap_or_else(|_| value_of(commands, b));
             insert_into_dict(identifier, a & b);
             return a & b;
         }
-        else if line.contains("OR") {
-            let (a, b) = (tokens[0], tokens[2]);
+        else if operator == "OR" {
             let a = a.parse().unwrap_or_else(|_| value_of(commands, a));
             let b = b.parse().unwrap_or_else(|_| value_of(commands, b));
             insert_into_dict(identifier, a | b);
             return a | b;
         }
-        else if line.contains("LSHIFT") {
-            let (a, b) = (tokens[0], tokens[2]);
+        else if operator == "LSHIFT" {
             if let ( Ok(a), Ok(b) ) = ( a.parse::<u16>(), b.parse::<u16>() ) {
                 insert_into_dict(identifier, a << b);
                 return a << b;
@@ -65,8 +65,7 @@ fn value_of(commands: &str, identifier: &str) -> u16 {
                 return value_of(commands, a) << b.parse::<u16>().unwrap();
             }
         }
-        else if line.contains("RSHIFT") {
-            let (a, b) = (tokens[0], tokens[2]);
+        else { // RSHIFT
             if let ( Ok(a), Ok(b) ) = ( a.parse::<u16>(), a.parse::<u16>() ) {
                 insert_into_dict(identifier, a >> b);
                 return a >> b;
@@ -76,7 +75,7 @@ fn value_of(commands: &str, identifier: &str) -> u16 {
         }
     }
     
-    panic!("yabe");
+    panic!("Yabe!");
 }
 
 fn _test() {

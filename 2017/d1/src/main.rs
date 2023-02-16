e5c2097a9c7d67b5cbf4a664bdcce091ff6cb39f
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-02-16 06:14:03 -0300

    2017-day1: Solve part 1 and 2

---
+++ 2017/d1/src/main.rs
@@ -0,0 +1,26 @@
+#![allow(unused)]
+
+fn main() {
+    let input = include_str!("..\\input.txt");
+    let digits = input.bytes().map(|b| (b as char).to_digit(10).unwrap()).collect::<Vec<_>>();
+
+    let mut sum = 0;
+    for i in 0..digits.len()-1 {
+        if digits[i] == digits[i+1] {
+            sum += digits[i];
+        }
+    }
+    if digits[0] == *digits.last().unwrap() { sum += digits[0] };
+    println!("Part 1: {sum}");
+
+    // part 2
+    let len = digits.len();
+    let half = digits.len() / 2;
+    let mut sum = 0;
+    for i in 0..len-1 {
+        if digits[i] == digits[(i + half) % len] {
+            sum += digits[i];
+        }
+    }
+    println!("Part 2: {sum}");
+}

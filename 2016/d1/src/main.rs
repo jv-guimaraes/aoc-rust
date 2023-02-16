387d7ed8f2fcd22c9227f6a563ea6d26855ccc44
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-02-15 21:28:06 -0300

    2016-day1: Solve part 2

--- 2016/d1/src/main.rs
+++ 2016/d1/src/main.rs
@@ -1,6 +1,4 @@
-// #![allow(unused)]
-
-use std::fmt::Display;
+use std::{fmt::Display, collections::BTreeSet};
 
 #[derive(Debug, Clone, Copy)]
 enum Dir {
@@ -30,7 +28,7 @@ impl Dir {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Coord {
     x: i32,
     y: i32,
@@ -69,9 +67,43 @@ fn calculate_coordinates(commands: &str) -> Coord {
     coord
 }
 
+fn visit_twice(commands: &str) -> Coord {
+    let mut dir = Dir::North;
+    let mut coord = Coord {x: 0, y: 0};
+    let mut visited = BTreeSet::new();
+
+    for command in commands.split(", ") {
+        let letter = &command[..1];
+        let number = command[1..command.len()].parse::<i32>().unwrap();
+        
+        dir = if letter == "R" { dir.turn_right() } else { dir.turn_left() };
+
+        for _ in 0..number {
+            match dir {
+                Dir::North => coord.y += 1,
+                Dir::East => coord.x += 1,
+                Dir::South => coord.y -= 1,
+                Dir::West => coord.x -= 1,
+            }
+    
+            if !visited.insert(coord) {
+                return coord;
+            }
+        }
+
+    }
+    
+    coord
+}
+
 fn main() {
     let input = include_str!("..\\input.txt");
     let final_coord = calculate_coordinates(input);
     println!("Final coordinate: {}", final_coord);
-    println!("Part 1: {}", final_coord.distance());
+    println!("Distance: {}", final_coord.distance());
+
+    // part 2
+    let final_coord = visit_twice(input);
+    println!("\nVisited twice: {}", final_coord);
+    println!("Distance: {}", final_coord.distance());
 }

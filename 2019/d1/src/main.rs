76378e46445acff997f2aaed8b0cb93c3018a6ad
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-02-19 11:06:08 -0300

    2019-d1 and 2019-d2: Solve day 1 and 2

---
+++ 2019/d1/src/main.rs
@@ -0,0 +1,33 @@
+#![allow(unused)]
+
+const INPUT: &str = include_str!("..\\input.txt");
+
+fn calculate_fuel(x: i32, acc: i32) -> i32 {
+    let result = x / 3 - 2;
+    if result <= 0 {
+        acc
+    } else {
+        calculate_fuel(result, acc + result)
+    }
+}
+
+fn part1() {
+    let mut total = 0;
+    for line in INPUT.lines().map(|x| x.parse::<i32>().unwrap()) {
+        total += line / 3 - 2;
+    }
+    println!("Part 1: {}", total);
+}
+
+fn part2() {
+    let total: i32 = INPUT
+        .lines()
+        .map(|x| calculate_fuel(x.parse::<i32>().unwrap(), 0))
+        .sum();
+    println!("Part 2: {total}");
+}
+
+fn main() {
+    part1();
+    part2();
+}
---
+++ 2019/d2/src/main.rs
@@ -0,0 +1,59 @@
+#![allow(unused)]
+
+const INPUT: &str = include_str!("..\\input.txt");
+
+fn load_code(noun: usize, verb: usize) -> Vec<usize> {
+    assert!((0..=99).contains(&noun) && (0..=99).contains(&verb));
+    let mut code: Vec<usize> = INPUT
+        .trim_end()
+        .split(',')
+        .map(|x| x.parse().unwrap())
+        .collect();
+    (code[1], code[2]) = (noun, verb);
+    code
+}
+
+fn execute_code(code: &mut [usize]) {
+    for i in (0..code.len()).step_by(4) {
+        let opcode = code[i];
+        if opcode == 99 { // END
+            return;
+        }
+        let (arg1, arg2, arg3) = (code[i+1], code[i+2], code[i+3]);
+        if opcode == 1 { // ADDITION
+            code[arg3] = code[arg1] + code[arg2];
+        }
+        else if opcode == 2 { // MULTIPLICATION
+            code[arg3] = code[arg1] * code[arg2];
+        } else {
+            panic!("Incorrect opcode: {opcode}");
+        }
+    }
+}
+
+fn run_with_input(noun: usize, verb: usize) -> usize {
+    let mut code = load_code(noun, verb);
+    execute_code(&mut code);
+    code[0]
+}
+
+fn part1() {
+    let mut code = load_code(12, 2);
+    execute_code(&mut code);
+    println!("Part 1: {:?}", code[0]);
+}
+
+fn part2() {
+    for noun in (0..=99) {
+        for verb in (0..=99) {
+            if run_with_input(noun, verb) == 19690720 {
+                println!("Part 2: {}", 100 * noun + verb);
+            }
+        }
+    }
+}
+
+fn main() {
+    part1();
+    part2();
+}

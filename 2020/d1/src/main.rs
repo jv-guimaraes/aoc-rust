6de1e8394060ef31758fe7d12a45422c5c4aa206
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-02-20 22:52:30 -0300

    2020-d1: Solve part 2

--- 2020/d1/src/main.rs
+++ 2020/d1/src/main.rs
@@ -1,7 +1,6 @@
 const INPUT: &str = include_str!("..\\input.txt");
 
-fn part1() {
-    let entries: Vec<u32> = INPUT.lines().map(|e| e.parse().unwrap()).collect();
+fn part1(entries: &[u32]) {
     for e1 in entries.iter() {
         for e2 in entries.iter() {
             if e1 + e2 == 2020 {
@@ -12,6 +11,21 @@ fn part1() {
     }
 }
 
+fn part2(entries: &[u32]) {
+    for e1 in entries.iter() {
+        for e2 in entries.iter() {
+            for e3 in entries.iter() {
+                if e1 + e2 + e3 == 2020 {
+                    println!("Part 2: {} * {} * {} = {}", e1, e2, e3, e1 * e2 * e3);
+                    return;
+                }
+            }
+        }
+    }
+}
+
 fn main() {
-    part1();
+    let entries: Vec<u32> = INPUT.lines().map(|e| e.parse().unwrap()).collect();
+    part1(&entries);
+    part2(&entries);
 }

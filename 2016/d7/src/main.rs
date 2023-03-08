fde2c072f5e53de5705896dbf6cbbbf381d44a7f
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-03-08 20:45:54 -0300

    2016-d7: Solve part 2

--- 2016/d7/src/main.rs
+++ 2016/d7/src/main.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]
 use itertools::Itertools;
 use regex_macro::regex;
 
@@ -20,26 +19,37 @@ fn supports_tls(text: &str) -> bool {
     false
 }
 
+fn supernet_hypernet_split(text: &str) -> (String, String) {
+    let re = regex!(r"(\[.*?\])");
+    let supernet = re.replace_all(text, "--").to_string();
+    let mut hypernet = String::new();
+    for find in re.find_iter(text) {
+        let slice = &text[find.start()..find.end()];
+        hypernet.push_str(slice);
+    }
+    (supernet, hypernet)
+}
+
 fn supports_ssl(text: &str) -> bool {
-    // whitin brackers
-    
-
+    let (supernet, hypernet) = supernet_hypernet_split(text);
     // outside brackets
-
-    todo!()
+    for (a, b, c) in supernet.bytes().tuple_windows() {
+        if a == c && a != b {
+            // whitin brackers
+            for (i, j, k) in hypernet.bytes().tuple_windows() {
+                if i == b && k == b && j == a {
+                    return true;
+                }
+            }
+        }
+    }
+    false
 }
 
 fn main()  {
-    let a = "abba[mnop]qrst";
-    assert!(supports_tls(a));
-    assert!(!supports_tls("abcd[bddb]xyyx"));
-    assert!(!supports_tls("aaaa[qwer]tyui"));
-    assert!(supports_tls("ioxxoj[asdfgh]zxcvbn"));
-
     let r = INPUT.lines().filter(|s| supports_tls(s)).count();
     println!("Part 1: {}", r);
 
-    let re = regex!(r"(\[.*?\])");
-    println!("{}", re.replace_all("abba[mnop]qrst", ""));
-
+    let r = INPUT.lines().filter(|s| supports_ssl(s)).count();
+    println!("Part 2: {}", r);
 }

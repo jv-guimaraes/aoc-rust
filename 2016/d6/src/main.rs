38f69d0b405ab34d32b3878145149f40f095ac68
jv-guimaraes <joao.guimaraes@aluno.uepb.edu.br>
2023-03-08 11:42:09 -0300

    2016-d6: Improve common variable name

--- 2016/d6/src/main.rs
+++ 2016/d6/src/main.rs
@@ -2,34 +2,34 @@ use itertools::Itertools;
 
 const INPUT: &str = include_str!("..\\input.txt");
 
-fn most_common(byte_slices: &[&[u8]], index: usize) -> char {
-    let dict = byte_slices.iter().map(|x| x[index]).counts();
+fn most_common(lines: &[&[u8]], index: usize) -> char {
+    let dict = lines.iter().map(|x| x[index]).counts();
     dict.into_iter().max_by_key(|x| x.1).unwrap().0 as char
 }
 
-fn least_common(byte_slices: &[&[u8]], index: usize) -> char {
-    let dict = byte_slices.iter().map(|x| x[index]).counts();
+fn least_common(lines: &[&[u8]], index: usize) -> char {
+    let dict = lines.iter().map(|x| x[index]).counts();
     dict.into_iter().min_by_key(|x| x.1).unwrap().0 as char
 }
 
-fn part1(byte_slices: &[&[u8]]) {
+fn part1(lines: &[&[u8]]) {
     print!("Part 1: ");
-    for i in 0..byte_slices[0].len() {
-        print!("{}", most_common(byte_slices, i));
+    for i in 0..lines[0].len() {
+        print!("{}", most_common(lines, i));
     }
     println!()
 }
 
-fn part2(byte_slices: &[&[u8]]) {
+fn part2(lines: &[&[u8]]) {
     print!("Part 2: ");
-    for i in 0..byte_slices[0].len() {
-        print!("{}", least_common(byte_slices, i));
+    for i in 0..lines[0].len() {
+        print!("{}", least_common(lines, i));
     }
     println!()
 }
 
 fn main()  {
-    let byte_slices = INPUT.lines().map(|x| x.as_bytes()).collect_vec();
-    part1(&byte_slices);
-    part2(&byte_slices);
+    let lines = INPUT.lines().map(|x| x.as_bytes()).collect_vec();
+    part1(&lines);
+    part2(&lines);
 }
